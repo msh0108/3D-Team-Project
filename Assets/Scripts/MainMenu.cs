c81381ca49d96c2f9c6074ef13dd350b9b9b6759
msh0108 <audtmdgur@naver.com>
2022-07-11 03:40:43 +0900

    0710 interior, mainmenu UGUI

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStart()
+    {
+        Debug.Log("게임시작");
+    }
+
+    public void OnClickAbout()
+    {
+        Debug.Log("게임설명");
+    }
+
+    public void OnClickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
